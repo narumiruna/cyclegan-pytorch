173cb4da2465a55216a0c4b8ee865d6cbe19b8b4
なるみ <weaper@gmail.com>
2019-07-16 14:46:36 +0800

    Rename class

--- cyclegan/models/resnet.py
+++ cyclegan/models/resnet.py
@@ -1,57 +1,47 @@
-import torch
 from torch import nn
 
 
-class Identity(nn.Module):
+class ConvINLeakyReLU(nn.Sequential):
 
-    def __init__(self, *args, **kwargs):
-        super(Identity, self).__init__()
-
-    def forward(self, *inputs):
-        return inputs
-
-
-class ConvBNReLU(nn.Sequential):
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, negative_slope=0.2):
         padding = (kernel_size - 1) // 2
         layers = [
             nn.ReflectionPad2d(padding),
-            nn.Conv2d(in_planes, out_planes, kernel_size, stride=stride, padding=0, bias=False),
-            nn.BatchNorm2d(out_planes),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride=stride, padding=0),
+            nn.InstanceNorm2d(out_planes),
+            nn.LeakyReLU(negative_slope, inplace=True),
         ]
-        super(ConvBNReLU, self).__init__(*layers)
+        super(ConvINLeakyReLU, self).__init__(*layers)
 
 
-class ConvTranposeBNReLU(nn.Sequential):
+class DeconvINLeakyReLU(nn.Sequential):
 
-    def __init__(self, in_planes, out_planes):
+    def __init__(self, in_planes, out_planes, negative_slope=0.2):
         layers = [
-            nn.ConvTranspose2d(in_planes, out_planes, 3, stride=2, padding=1, output_padding=1, bias=False),
-            nn.BatchNorm2d(out_planes),
-            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(in_planes, out_planes, 3, stride=2, padding=1, output_padding=1),
+            nn.InstanceNorm2d(out_planes),
+            nn.LeakyReLU(negative_slope, inplace=True),
         ]
-        super(ConvTranposeBNReLU, self).__init__(*layers)
+        super(DeconvINLeakyReLU, self).__init__(*layers)
 
 
 class ResnetGenerator(nn.Sequential):
 
     def __init__(self):
         layers = [
-            ConvBNReLU(3, 64, 7, 1),
-            ConvBNReLU(64, 128, 3, stride=2),
-            ConvBNReLU(128, 256, 3, stride=2),
+            ConvINLeakyReLU(3, 64, 7, 1),
+            ConvINLeakyReLU(64, 128, 3, stride=2),
+            ConvINLeakyReLU(128, 256, 3, stride=2),
             ResidualBlock(256, 3),
             ResidualBlock(256, 3),
             ResidualBlock(256, 3),
             ResidualBlock(256, 3),
             ResidualBlock(256, 3),
             ResidualBlock(256, 3),
-            ConvTranposeBNReLU(256, 128),
-            ConvTranposeBNReLU(128, 64),
+            DeconvINLeakyReLU(256, 128),
+            DeconvINLeakyReLU(128, 64),
             nn.ReflectionPad2d(3),
-            nn.Conv2d(64, 3, 7, bias=True),
+            nn.Conv2d(64, 3, 7),
             nn.Tanh(),
         ]
         super(ResnetGenerator, self).__init__(*layers)
@@ -59,36 +49,21 @@ class ResnetGenerator(nn.Sequential):
 
 class ResidualBlock(nn.Module):
 
-    def __init__(self, channels, kernel_size):
+    def __init__(self, channels, kernel_size, negative_slope=0.2):
         super(ResidualBlock, self).__init__()
         padding = (kernel_size - 1) // 2
         self.features = nn.Sequential(
             nn.ReflectionPad2d(padding),
-            nn.Conv2d(channels, channels, kernel_size, stride=1, bias=False),
-            nn.BatchNorm2d(channels),
-            nn.ReLU(inplace=True),
+            nn.Conv2d(channels, channels, kernel_size, stride=1),
+            nn.InstanceNorm2d(channels),
+            nn.LeakyReLU(negative_slope, inplace=True),
             nn.ReflectionPad2d(padding),
-            nn.Conv2d(channels, channels, kernel_size, stride=1, bias=False),
-            nn.BatchNorm2d(channels),
+            nn.Conv2d(channels, channels, kernel_size, stride=1),
+            nn.InstanceNorm2d(channels),
         )
 
     def forward(self, x):
         return x + self.features(x)
 
-
-def numel(model):
-    return sum(p.numel() for p in model.parameters())
-
-
-def main():
-    model = ResnetGenerator()
-
-    x = torch.randn(1, 3, 224, 224)
-    with torch.no_grad():
-        y = model(x)
-
-    print(y.size())
-    print(numel(model))
-
-if __name__ == '__main__':
-    main()
+    def numel(self):
+        return sum(p.numel() for p in self.parameters())
